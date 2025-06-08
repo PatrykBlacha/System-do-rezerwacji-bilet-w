f7b43b8bcc40b2e0ca1c77a1c74d9bd027e7354b
Maciej Trznadel <maciej.trznadel24@gmail.com>
2025-06-08 22:12:46 +0200

    Fixed views to current database

--- tickets_system/tickets_app/admin.py
+++ tickets_system/tickets_app/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
-from .models import Event, Ticket, Client
+from .models import Event, Ticket, Participant
 
 admin.site.register(Event)
 
 admin.site.register(Ticket)
 
-admin.site.register(Client)
+admin.site.register(Participant)
