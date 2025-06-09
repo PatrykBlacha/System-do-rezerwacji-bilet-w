16ae2541a10e973371267c584d903befed48513b
Maciej Trznadel <maciej.trznadel24@gmail.com>
2025-06-09 03:49:01 +0200

    fixed forms and added documentation

--- tickets_system/tickets_app/forms.py
+++ tickets_system/tickets_app/forms.py
@@ -6,8 +6,7 @@ class CustomUserCreationForm(UserCreationForm):
     first_name = forms.CharField(max_length=64, required=True)
     last_name = forms.CharField(max_length=64, required=True)
     email = forms.EmailField(required=True)
-    address = forms.CharField(max_length=128, required=False)  # dodaj adres (opcjonalnie)
 
     class Meta:
         model = User
-        fields = ("username", "first_name", "last_name", "email", "address", "password1", "password2")
+        fields = ("username", "first_name", "last_name", "email", "password1", "password2")
